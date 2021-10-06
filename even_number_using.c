c22e8e17974057719d719b2d7eba3d8763c8fc5e
himanshugjr57 <87509081+himanshugjr57@users.noreply.github.com>
2021-10-06 08:13:04 +0530

    Update even_number_using.c

--- even_number_using.c
+++ even_number_using.c
@@ -2,7 +2,8 @@
 int main()
 {
     printf("The first 10 even number are: ");
-    for (int i = 1; i <= 10; i++)
+    
+    for (int i = 1; i <= 20; i++)
     {
         if (i % 2 == 0)
         {
@@ -10,4 +11,4 @@ int main()
         }
     }
     return 0;
-}sni
+}
