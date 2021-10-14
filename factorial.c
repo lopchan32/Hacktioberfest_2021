794786790bb38bb748c66da236557360b5c0adbe
Jash hinger <72138103+jash458@users.noreply.github.com>
2021-10-14 19:51:10 +0530

    Add factorial code.
    please accept my PR.

--- factorial.c
+++ factorial.c
@@ -12,3 +12,21 @@ int main()
         printf("the factor of %d is: %d", number, temp);
 return 0;
 }
+
+
+
+#include<stdio.h>
+ int main(){
+     
+     int n, temp=1;
+     printf("Enter number: ");
+     scanf("%d", &n);
+     
+     while(i <= n){
+         temp*=i;
+         i++;
+     }
+     printf("The factroial of %d is : %d", n,temp);
+     return 0;
+     
+ }
